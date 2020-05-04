f7c3b8305584a9e264b30bbed83b6823c5e408e4
Simón Cebrero <62322984+simoncebrero095@users.noreply.github.com>
2020-05-03 21:21:56 -0300

    importarStore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//NgRx
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/hijo/nieto/nieto.component';
+import { contadorReducer } from './contador/contador.reducer';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { NietoComponent } from './contador/hijo/nieto/nieto.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ contador: contadorReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
