2fc780ee94d31a546f4549509e65a56b3f67a00d
Simón Cebrero <62322984+simoncebrero095@users.noreply.github.com>
2020-05-04 16:49:37 -0300

    contadorReducer_InitialState

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,22 +1,30 @@
-import { Action } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { incrementar, decrementar } from './contador.actions';
 
-export function contadorReducer( state: number = 10, action:Action){
-    
-    switch( action.type ) {
+//export function contadorReducer( state: number = 10, action:Action){
+   
+    // switch( action.type ) {
         
-        case incrementar.type:
-            return state + 1;
+    //     case incrementar.type:
+    //         return state + 1;
         
-        case decrementar.type:
-                return state - 1; 
+    //     case decrementar.type:
+    //             return state - 1; 
 
-        default:
-            return state;
+    //     default:
+    //         return state;    
+  
 
 
 
+export const initialState = 20;
+ 
+    const _contadorReducer = createReducer(initialState,
+      on(incrementar, state => state + 1),
+      on(decrementar, state => state - 1),
+     
+    );
 
-    }
-
-}
+    export function contadorReducer(state, action) {
+        return _contadorReducer(state, action);
+      }
