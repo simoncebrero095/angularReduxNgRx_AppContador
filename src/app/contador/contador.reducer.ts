7518c30befa3c057faead6e17a2f1e19268378a2
Simón Cebrero <62322984+simoncebrero095@users.noreply.github.com>
2020-05-04 22:19:24 -0300

    addReset.Reducer

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from '@ngrx/store';
-import { incrementar, decrementar } from './contador.actions';
+import * as actions from '../contador/contador.actions'; 
 
 //export function contadorReducer( state: number = 10, action:Action){
    
@@ -20,9 +20,11 @@ import { incrementar, decrementar } from './contador.actions';
 export const initialState = 20;
  
     const _contadorReducer = createReducer(initialState,
-      on(incrementar, state => state + 1),
-      on(decrementar, state => state - 1),
-     
+      on(actions.incrementar, state => state + 1),
+      on(actions.decrementar, state => state - 1),
+      on(actions.multiplicar,( state, {numero}) => state * numero),
+      on(actions.dividir,(state, {numero}) => state / numero),
+      on(actions.reset,(state => state = 0))
     );
 
     export function contadorReducer(state, action) {
