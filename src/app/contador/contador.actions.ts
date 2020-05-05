80918c0b0d65ff212e7f5d3c2ad8ba827fabe596
Simón Cebrero <62322984+simoncebrero095@users.noreply.github.com>
2020-05-04 22:19:10 -0300

    addReset.Actions

--- src/app/contador/contador.actions.ts
+++ src/app/contador/contador.actions.ts
@@ -1,4 +1,11 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 
 export const incrementar = createAction('[contador] Incrementar');
 export const decrementar = createAction('[contador] Decrementar');
+export const multiplicar = createAction('[contador] Multiplicar',
+props<{numero: number}>()
+);
+export const dividir = createAction('[contador] Dividir' ,
+props<{numero: number}>()
+);
+export const reset = createAction('[contador] Reset');
